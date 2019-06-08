41f82a1155b705310ce9a208d0c2ad23348cb99c
Fister Gutierrez <14470418@itcampeche.edu.mx>
2019-06-08 17:34:42 -0500

    Integrando Landing Page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('APP.NAME', 'LACORE') }}</title>
+    <title>{{ config('APP.NAME', 'LAXCORE') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"></script>
